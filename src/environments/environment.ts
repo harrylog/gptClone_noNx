812748df5bdea16d4c8151a07d666e7108d5dff0
harry log <harry8log@gmail.com>
2024-10-22 14:53:23 +0300

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: false,
    openaiApiKey1:''
-  //   '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',
 
 
 
